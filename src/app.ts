3b1da8964d33925672f3fbfb371c3c9b8deabd41
Franco Trelles <franco.trellesm@gmail.com>
2021-01-21 13:10:38 -0500

    Autobind decorator for configuring input elements

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,28 @@
 // Code goes here!
 
+//Autobind decorator
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  console.log("THIS IS ORIGINAL METHOD", originalMethod);
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+
+  return adjDescriptor;
+}
+
 class ProjectInput {
   //We are giving our properties types
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     //We use type casting (as HTMLElement) due to Typescript not being able to look into our HTML File, but we know in this case
@@ -24,9 +42,33 @@ class ProjectInput {
 
     //Returns the first child node as an HTML Element
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
+    this.configure();
 
     //We run this method with every instance created
     this.attach();
+  }
+
+  @autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler);
   }
 
   private attach() {
