86f1f47cc0e668109f215d356bb62725ea9c80a0
Franco Trelles <franco.trellesm@gmail.com>
2021-01-21 14:46:53 -0500

    Created class for handling project lists

--- src/app.ts
+++ src/app.ts
@@ -26,11 +26,11 @@ function validate(input: Validatable) {
   }
 
   if (input.min != null && typeof input.value === "number") {
-    isValid = isValid && input.value > input.min;
+    isValid = isValid && input.value >= input.min;
   }
 
   if (input.max != null && typeof input.value === "number") {
-    isValid = isValid && input.value < input.max;
+    isValid = isValid && input.value <= input.max;
   }
 
   return isValid;
@@ -39,7 +39,6 @@ function validate(input: Validatable) {
 //Autobind decorator
 function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   const originalMethod = descriptor.value;
-  console.log("THIS IS ORIGINAL METHOD", originalMethod);
   const adjDescriptor: PropertyDescriptor = {
     configurable: true,
     get() {
@@ -51,6 +50,43 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   return adjDescriptor;
 }
 
+//Project List Class
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: "active" | "finished") {
+    this.templateElement = document.getElementById(
+      "project-list"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector("h2")!.textContent =
+      this.type.toUpperCase() + " PROJECTS";
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+}
+
+//Project Input Class
 class ProjectInput {
   //We are giving our properties types
   templateElement: HTMLTemplateElement;
@@ -159,3 +195,6 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+
+const activePrjList = new ProjectList("active");
+const finishedPrjList = new ProjectList("finished");
